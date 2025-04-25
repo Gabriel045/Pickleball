d4acd77da5743e101aa18799aba1878270bacc41
gabriel Cabrera <gabriel.cabera@gmail.com>
2025-04-25 16:17:52 -0400

    feat: Update WooCommerce account templates with new styles and layout adjustments

--- template-parts/my-videos.php
+++ template-parts/my-videos.php
@@ -38,25 +38,25 @@ foreach ($product_ids as $product_id) {
 ?>
 
 
-<div class="py-[30px] lg:py-[60px] px-[30px] lg:px-[80px]">
+<div class="">
     <h3 class="text-[24px] text-rich-black font-semibold">My Videos Library </h3>
     <div class="mt-10 flex flex-wrap gap-[2%] gap-y-[40px]">
         <?php foreach ($videos as $video) : ?>
-        <div class="w-full md:w-[49%] lg:w-[31.33%]">
-            <figure>
-                <img class="rounded-[10px]" src="<?php echo $video['image'] ?>">
-            </figure>
-            <div class="flex items-center gap-[10px] mt-6">
-                <span class="stars"></span>
-                <span class="text-[14px] text-[rgba(71,84,103,0.60)] font-medium leading-[24px]">5.0 (59
-                    Reviews)</span>
+            <div class="w-full md:w-[49%] lg:w-[31.33%]">
+                <figure>
+                    <img class="rounded-[10px]" src="<?php echo $video['image'] ?>">
+                </figure>
+                <div class="flex items-center gap-[10px] mt-6">
+                    <span class="stars"></span>
+                    <span class="text-[14px] text-[rgba(71,84,103,0.60)] font-medium leading-[24px]">5.0 (59
+                        Reviews)</span>
+                </div>
+                <p class="text-[20px] text-rich-black font-semibold"><?php echo $video['title'] ?></p>
+                <p class="mt-2 text-gray-paragrah"><?php echo $video['excerpt'] ?></p>
+                <a href="<?php echo esc_url(home_url('/course/' . $video["slug"])); ?>" class="btn">
+                    See Course
+                </a>
             </div>
-            <p class="text-[20px] text-rich-black font-semibold"><?php echo $video['title'] ?></p>
-            <p class="mt-2 text-gray-paragrah"><?php echo $video['excerpt'] ?></p>
-            <a href="<?php echo esc_url(home_url('/course/' . $video["slug"])); ?>" class="btn">
-                Ver Curso
-            </a>
-        </div>
         <?php endforeach; ?>
     </div>
 </div>
--- woocommerce/myaccount/navigation.php
+++ woocommerce/myaccount/navigation.php
@@ -27,13 +27,13 @@ do_action('woocommerce_before_account_navigation');
     aria-label="<?php esc_html_e('Account pages', 'woocommerce'); ?>">
     <ul>
         <?php foreach (wc_get_account_menu_items() as $endpoint => $label) : ?>
-        <li
-            class="mb-[15px] text-rich-black text-[18px] font-semibold <?php echo wc_get_account_menu_item_classes($endpoint); ?>">
-            <a href="<?php echo esc_url(wc_get_account_endpoint_url($endpoint)); ?>"
-                <?php echo wc_is_current_account_menu_item($endpoint) ? 'aria-current="page"' : ''; ?>>
-                <?php echo esc_html($label); ?>
-            </a>
-        </li>
+            <li
+                class="mb-[15px] text-rich-black text-[18px] font-semibold <?php echo wc_get_account_menu_item_classes($endpoint); ?>">
+                <a href="<?php echo esc_url(wc_get_account_endpoint_url($endpoint)); ?>"
+                    <?php echo wc_is_current_account_menu_item($endpoint) ? 'aria-current="page"' : ''; ?>>
+                    <?php echo esc_html($label); ?>
+                </a>
+            </li>
         <?php endforeach; ?>
     </ul>
     <div>
@@ -43,6 +43,21 @@ do_action('woocommerce_before_account_navigation');
             </li>
             <li class="woocommerce-settings mb-[15px] text-rich-black text-[18px] font-semibold ">
                 <a href="/my-account/edit-account/">Settings</a>
+            </li>
+            <li class="flex justify-between">
+                <div class="flex flex-col">
+                    <span
+                        class="text-[18px] font-[600] text-rich-black"><?php echo esc_html(wp_get_current_user()->display_name); ?></span>
+                    <span
+                        class="text-gray-paragrah text-[14px]"><?php echo esc_html(wp_get_current_user()->user_email); ?></span>
+                </div>
+                <a href="<?php echo esc_url(wp_logout_url(home_url())); ?>">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" viewBox="0 0 21 21" fill="none">
+                        <path
+                            d="M13.8333 14.8893L18 10.7227M18 10.7227L13.8333 6.55599M18 10.7227H8M8 3.22266H7C5.59987 3.22266 4.8998 3.22266 4.36502 3.49514C3.89462 3.73482 3.51217 4.11727 3.27248 4.58768C3 5.12246 3 5.82252 3 7.22266V14.2227C3 15.6228 3 16.3229 3.27248 16.8576C3.51217 17.328 3.89462 17.7105 4.36502 17.9502C4.8998 18.2227 5.59987 18.2227 7 18.2227H8"
+                            stroke="#667085" stroke-width="1.66667" stroke-linecap="round" stroke-linejoin="round" />
+                    </svg>
+                </a>
             </li>
 
         </ul>
