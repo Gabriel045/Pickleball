137949b855644edb5dd8dd868bfc7588045ef582
gabriel Cabrera <gabriel.cabera@gmail.com>
2025-05-05 15:37:07 -0400

    Implement code changes to enhance functionality and improve performance

--- blocks/faq/faq.php
+++ blocks/faq/faq.php
@@ -1,20 +1,21 @@
 <?php
 
 $title    = get_field('title');
-$faqs    = get_field('faqs');
+$faqs = !empty(get_field('faqs')) ? get_field('faqs') : $args['faqs'];
 
 ?>
 
 <section id="faq">
     <div class="block_content py-[60px] lg:py-[100px] px-[30px] lg:px-[300px]">
-        <h2 class="text-rich-black text-[20px] lg:text-[24px] font-semibold text-center"><?php echo $title ?></h2>
+        <h2 class="text-rich-black text-[20px] lg:text-[24px] font-semibold text-center">
+            <?php echo !empty($title) ? $title : 'Frequently Asked Questions' ?></h2>
         <div class="pt-8 flex flex-col gap-6">
             <?php foreach ($faqs as $key => $card) : ?>
-            <details class="pb-8 border-b border-[#E0E0E0] ">
-                <summary class="text-[18px] text-rich-black font-medium cursor-pointer"><?php echo $card["title"] ?>
-                </summary>
-                <div class="text-gray-paragrah mt-2"><?php echo $card["content"] ?></div>
-            </details>
+                <details class="pb-8 border-b border-[#E0E0E0] ">
+                    <summary class="text-[18px] text-rich-black font-medium cursor-pointer"><?php echo $card["title"] ?>
+                    </summary>
+                    <div class="text-gray-paragrah mt-2"><?php echo $card["content"] ?></div>
+                </details>
             <?php endforeach; ?>
         </div>
     </div>
--- single-product.php
+++ single-product.php
@@ -1,52 +1,53 @@
 <?php
 
 get_header();
-?>
 
+global $post;
+$product = wc_get_product($post->ID);
+
+$product_price = $product ? $product->get_price_html() : '';
+$product_image = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'full')[0];
+$trailer_video =  get_field('trailer_video', $post->ID)["url"];
+$description_list_title = get_field('description_list_title', $post->ID);
+$description_list = get_field('description_list', $post->ID);
+$details_content = get_field('details_content', $post->ID);
+$flow_chart_content = get_field('flow_chart_content', $post->ID);
+$contents = get_field('contents', $post->ID);
+$text_image = get_field('text_image', $post->ID);
+$text_image_2 = get_field('text_image_2', $post->ID);
+$best_seller_title = get_field('best_seller_title', $post->ID);
+$faqs = get_field('faqs', $post->ID); ?>
 
 <main>
     <section>
         <div class="block_content">
-            <div class="flex">
+            <div class="flex lg:flex-nowrap flex-wrap lg:flex-row flex-col-reverse">
                 <!-- Right Side  -->
-                <div class="w-1/2">
-                    <?php
-                    global $post;
-                    $product = wc_get_product($post->ID);
+                <div class="w-full lg:w-1/2">
+                    <div class="hidden lg:block">
+                        <?php
+                        if (!empty($trailer_video)) { ?>
+                            <div class="relative cursor-pointer">
+                                <svg id="play-icon" class="absolute top-1/2 left-1/2"
+                                    style="transform: translate(-50%, -50%);" xmlns="http://www.w3.org/2000/svg" width="68"
+                                    height="78" viewBox="0 0 68 78" fill="none">
+                                    <path d="M67.3047 39L0.847416 77.3691L0.847419 0.630879L67.3047 39Z" fill="white" />
+                                </svg>
 
-                    $product_price = $product ? $product->get_price_html() : '';
-                    $product_image = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'full')[0];
-                    $trailer_video =  get_field('trailer_video', $post->ID)["url"];
-                    $description_list_title = get_field('description_list_title', $post->ID);
-                    $description_list = get_field('description_list', $post->ID);
-                    $details_content = get_field('details_content', $post->ID);
-                    $flow_chart_content = get_field('flow_chart_content', $post->ID);
-                    $contents = get_field('contents', $post->ID);
-                    $text_image = get_field('text_image', $post->ID);
-                    $text_image_2 = get_field('text_image_2', $post->ID);
-                    $best_seller_title = get_field('best_seller_title', $post->ID);
-
-
-                    if (!empty($trailer_video)) { ?>
-                        <div class="relative cursor-pointer">
-                            <svg id="play-icon" class="absolute top-1/2 left-1/2" style="transform: translate(-50%, -50%);"
-                                xmlns="http://www.w3.org/2000/svg" width="68" height="78" viewBox="0 0 68 78" fill="none">
-                                <path d="M67.3047 39L0.847416 77.3691L0.847419 0.630879L67.3047 39Z" fill="white" />
-                            </svg>
-
-                            <video id="product-video" class="h-auto aspect-video object-cover" controls
-                                poster="<?php echo esc_url($product_image) ?>">
-                                <source src="<?php echo esc_url($trailer_video) ?>" type="video/mp4">
-                                Your browser does not support the video tag.
-                            </video>
-                        </div>
-                    <?php  } else {
-                        echo '<img src="' . esc_url($product_image) . '" alt="' . esc_attr(get_the_title($product_id)) . '"
+                                <video id="product-video" class="h-auto aspect-[1.46] object-cover" controls
+                                    poster="<?php echo esc_url($product_image) ?>">
+                                    <source src="<?php echo esc_url($trailer_video) ?>" type="video/mp4">
+                                    Your browser does not support the video tag.
+                                </video>
+                            </div>
+                        <?php  } else {
+                            echo '<img src="' . esc_url($product_image) . '" alt="' . esc_attr(get_the_title($product_id)) . '"
                         class="featured-image">';
-                    } ?>
-                    <div class="bg-[#F9FAFB] p-[60px]">
+                        } ?>
+                    </div>
+                    <div class="bg-[#F9FAFB] lg:p-[60px] px-[40px] py-[60px]">
                         <div class="tabs"></div>
-                        <ul class="tab-list flex gap-4 border-b border-gray-300">
+                        <ul class="tab-list flex max-[1024px]:justify-between gap-0 lg:gap-4 border-b border-gray-300">
                             <li class="tab-item cursor-pointer py-2 px-4 text-gray-600 border-b-2 border-transparent active-tab"
                                 data-tab="tab1">Details</li>
                             <li class="tab-item cursor-pointer py-2 px-4 text-gray-600 border-b-2 border-transparent"
@@ -56,7 +57,7 @@ get_header();
                         </ul>
                         <div class="tab-content mt-4">
                             <div id="tab1" class="tab-pane active">
-                                <div class="text-gray-paragrah text-[18px] leading-[28px]">
+                                <div class="text-gray-paragrah tex-[16px] lg:text-[18px] leading-[28px]">
                                     <?php echo $details_content ?></div>
                             </div>
                             <div id="tab2" class="tab-pane hidden">
@@ -70,8 +71,8 @@ get_header();
                     </div>
                 </div>
                 <!-- Left Side -->
-                <div class="w-1/2 py-[64px] px-[60px]">
-                    <h1 class="text-[36px] font-[600] text-rich-black leading-[38px]">
+                <div class="w-full lg:w-1/2 py-[64px] px-[30px] lg:px-[60px]">
+                    <h1 class="text-[30px] lg:text-[36px] font-[600] text-rich-black leading-[38px]">
                         <?php echo esc_html(get_the_title($post->ID)); ?>
                     </h1>
                     <div class="flex items-center gap-[10px] my-4">
@@ -79,6 +80,27 @@ get_header();
                         <span class="text-[14px] text-[rgba(71,84,103,0.60)] font-medium leading-[24px]">
                             5.0 based on 543 reviews
                         </span>
+                    </div>
+                    <!-- video mobile -->
+                    <div class="block lg:hidden mb-[30px]">
+                        <?php
+                        if (!empty($trailer_video)) { ?>
+                            <div class="relative cursor-pointer">
+                                <svg id="play-icon" class="absolute top-1/2 left-1/2"
+                                    style="transform: translate(-50%, -50%);" xmlns="http://www.w3.org/2000/svg" width="50"
+                                    height="54" viewBox="0 0 68 78" fill="none">
+                                    <path d="M67.3047 39L0.847416 77.3691L0.847419 0.630879L67.3047 39Z" fill="white" />
+                                </svg>
+                                <video id="product-video" class="h-auto aspect-video object-cover rounded-[20px]" controls
+                                    playsinline muted poster="<?php echo esc_url($product_image) ?>">
+                                    <source src="<?php echo esc_url($trailer_video) ?>" type="video/mp4">
+                                    Your browser does not support the video tag.
+                                </video>
+                            </div>
+                        <?php  } else {
+                            echo '<img src="' . esc_url($product_image) . '" alt="' . esc_attr(get_the_title($product_id)) . '"
+                        class="featured-image">';
+                        } ?>
                     </div>
                     <p class="text-[18px] text-gray-paragrah leading-[28px]">
                         <?php echo esc_html(get_the_content($post->ID)); ?>
@@ -102,15 +124,15 @@ get_header();
                         <?php foreach ($description_list as $key => $item) {
 
                         ?>
-                            <li class="flex gap-2">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="28" height="29" viewBox="0 0 28 29"
-                                    fill="none">
+                            <li class="flex gap-3 lg:gap-2">
+                                <svg class="max-[1024px]:w-[72px]" xmlns="http://www.w3.org/2000/svg" width="28" height="29"
+                                    viewBox="0 0 28 29" fill="none">
                                     <rect y="0.5" width="28" height="28" rx="14" fill="#0B141D" fill-opacity="0.09" />
                                     <path fill-rule="evenodd" clip-rule="evenodd"
                                         d="M19.9476 9.12169L11.5943 17.1834L9.37763 14.815C8.9693 14.43 8.32763 14.4067 7.86096 14.7334C7.40596 15.0717 7.27763 15.6667 7.55763 16.145L10.1826 20.415C10.4393 20.8117 10.8826 21.0567 11.3843 21.0567C11.8626 21.0567 12.3176 20.8117 12.5743 20.415C12.9943 19.8667 21.0093 10.3117 21.0093 10.3117C22.0593 9.23836 20.7876 8.29336 19.9476 9.11002V9.12169Z"
                                         fill="#0B141D" />
                                 </svg>
-                                <p class="text-[18px] text-gray-paragrah leading-7"><?php echo $item["items"] ?></p>
+                                <div class="text-[18px] text-gray-paragrah leading-7"><?php echo $item["items"] ?></div>
                             </li>
                         <?php } ?>
                     </ul>
@@ -118,9 +140,9 @@ get_header();
             </div>
         </div>
     </section>
-    <section class="bg-[#F9FAFB]">
+    <section class="bg-[#F9FAFB] hidden lg:block">
         <div class="block_content py-[80px] px-[60px]">
-            <div class="flex gap-[100px]">
+            <div class="gap-[100px] flex">
                 <div class="w-1/2">
                     <div class="flex justify-between mb-10">
                         <h3 class="text-[24px] font-semibold text-rich-black">Course Content</h3>
@@ -170,16 +192,26 @@ get_header();
         </div>
     </section>
     <section>
-        <div class="block_content py-[80px] px-[60px]">
-            <div class="[&_h3]:text-rich-black [&_h3]:font-semibold [&_h3]:text-[24px] [&_h3]:leading-[28px]
-            [&_p]:text-[18px] [&_p]:text-gray-paragrah [&_p]:leading-7
-            [&_img]:h-[570px] [&_img]:object-cover">
+        <div class="block_content py-[80px] lg:px-[60px] px-[30px]">
+            <div class="[&_h3]:text-rich-black [&_h3]:font-semibold [&_h3]:text-[20px] [&_h3]:lg:text-[24px] [&_h3]:leading-[28px]
+            [&_p]:text-[16px] [&_p]:lg:text-[18px] [&_p]:text-gray-paragrah [&_p]:leading-7
+            [&_img]:lg:h-[570px] [&_img]:object-cover">
                 <?php echo $text_image_2 ?>
             </div>
         </div>
     </section>
     <?php
     get_template_part('/blocks/product-slider/product-slider', null, array('best_seller_title' => $best_seller_title));
+    ?>
+    <section id="reviews">
+        <div class="block_content px-[30px] lg:px-[60px] py-[80px]">
+            <?php
+            get_template_part('/template-parts/review-slider', null, array('slidesToShow' => '4'));
+            ?>
+        </div>
+    </section>
+    <?php
+    get_template_part('/blocks/faq/faq', null, array('faqs' => $faqs));
     ?>
 </main>
 
@@ -202,16 +234,34 @@ get_header();
     });
 
     document.addEventListener('DOMContentLoaded', function() {
-        const video = document.getElementById('product-video');
-        const playIcon = document.getElementById('play-icon');
+        const videos = document.querySelectorAll('#product-video');
+        const playIcons = document.querySelectorAll('#play-icon')
+        videos.forEach((video, index) => {
+            const playIcon = playIcons[index];
 
-        video.addEventListener('play', function() {
-            console.log("playdock")
-            playIcon.style.display = 'none';
-        });
+            video.addEventListener('touchstart', function() {
+                console.log("click")
+                if (video.paused) {
+                    video.play();
+                } else {
+                    video.pause();
 
-        video.addEventListener('pause', function() {
-            playIcon.style.display = 'block';
+                }
+            });
+
+            video.addEventListener('play', function() {
+                if (playIcon) {
+                    playIcon.style.display = 'none';
+                }
+            });
+
+            video.addEventListener('pause', function() {
+                if (playIcon) {
+                    playIcon.style.display = 'block';
+                }
+            });
+
+
         });
     });
 </script>
